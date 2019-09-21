bde0d656835c7576dccc5a8d046b86a58ec6e7f5
Jeff Garzik <jeff@bloq.com>
2019-09-21 18:45:34 +0000

    initial rev: rocket hello-world

---
+++ src/tests.rs
@@ -0,0 +1,9 @@
+use rocket::local::Client;
+
+#[test]
+fn hello_world() {
+    let rocket = rocket::ignite().mount("/", routes![super::hello]);
+    let client = Client::new(rocket).unwrap();
+    let mut response = client.get("/").dispatch();
+    assert_eq!(response.body_string(), Some("Hello, world!".into()));
+}
