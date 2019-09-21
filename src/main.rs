713ed121248a0ec837cecc58916f729424e2cfe0
Jeff Garzik <jeff@bloq.com>
2019-09-21 22:08:39 +0000

    simple get/put memdb api and implementation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,60 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(proc_macro_hygiene,decl_macro,rustc_private)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_derive;
 
 #[cfg(test)] mod tests;
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+use std::sync::Mutex;
+use std::collections::HashMap;
+
+use serde_json::json;
+
+use rocket::State;
+
+type DbMap = Mutex<HashMap<String, String>>;
+
+#[derive(Serialize, Deserialize)]
+struct DbEntry {
+    key: String,
+    val: String
+}
+
+#[get("/<dbkey>")]
+fn get(dbkey: String, map: State<'_, DbMap>) -> String {
+    let hashmap = map.lock().unwrap();
+    match hashmap.get(&dbkey) {
+        Some(val) => json!({"result": val}).to_string(),
+        _ => json!({"error": {
+            "code": -1,
+            "message": "key not found"
+            }}).to_string()
+    }
+}
+
+#[put("/<dbkey>/<dbval>")]
+fn put(dbkey: String, dbval: String, map: State<'_, DbMap>) -> String {
+    let mut hashmap = map.lock().unwrap();
+    hashmap.insert(dbkey, dbval);
+
+    json!({"result": true}).to_string()
+}
+
+#[catch(404)]
+fn not_found() -> String {
+    json!({"error": {
+        "code": -404,
+        "message": "URI not found"
+        }}).to_string()
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/1/db", routes![get, put])
+        .register(catchers![not_found])
+        .manage(Mutex::new(HashMap::<String, String>::new()))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket().launch();
 }
