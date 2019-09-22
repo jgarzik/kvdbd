945536a483342df26ec025855559655e542911bb
Jeff Garzik <jeff@bloq.com>
2019-09-22 17:56:38 +0000

    improve comments and formatting

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ struct ServerState {
     db: Db
 }
 
+// helper function, 404 not found
 fn err_not_found() -> Result<HttpResponse> {
     Ok(HttpResponse::build(StatusCode::NOT_FOUND)
         .content_type("application/json")
@@ -28,6 +29,7 @@ fn err_not_found() -> Result<HttpResponse> {
               "message": "not found"}}).to_string()))
 }
 
+// helper function, server error
 fn err_500() -> Result<HttpResponse> {
     Ok(HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR)
         .content_type("application/json")
@@ -37,13 +39,14 @@ fn err_500() -> Result<HttpResponse> {
               "message": "internal server error"}}).to_string()))
 }
 
+// helper function, success + json response
 fn ok_json(jval: serde_json::Value) -> Result<HttpResponse> {
     Ok(HttpResponse::build(StatusCode::OK)
         .content_type("application/json")
         .body(jval.to_string()))
 }
 
-/// simple index handler
+/// simple root index handler, describes our service
 #[get("/")]
 fn index(state: web::Data<ServerState>, req: HttpRequest) -> Result<HttpResponse> {
     println!("{:?}", req);
@@ -53,26 +56,26 @@ fn index(state: web::Data<ServerState>, req: HttpRequest) -> Result<HttpResponse
             "version": VERSION}))
 }
 
-/// GET data item
+/// GET data item.  key in URI path.  returned value as json response
 fn get(state: web::Data<ServerState>, req: HttpRequest, path: web::Path<(String,)>) -> Result<HttpResponse> {
     println!("{:?}", req);
 
     match state.db.get(path.0.clone()) {
         Ok(optval) => match optval {
             Some(val) => ok_json(json!({"result": String::from_utf8(val.to_vec()).unwrap()})),
-            None => err_not_found()
+            None => err_not_found()     // db: value not found
         },
-        Err(_e) => err_500()
+        Err(_e) => err_500()            // db: error
     }
 }
 
-/// PUT data item
+/// PUT data item.  key and value both in URI path.
 fn put(state: web::Data<ServerState>, req: HttpRequest, path: web::Path<(String,String)>) -> Result<HttpResponse> {
     println!("{:?}", req);
 
     match state.db.insert(path.0.as_str(), path.1.as_str()) {
         Ok(_optval) => ok_json(json!({"result": true})),
-        Err(_e) => err_500()
+        Err(_e) => err_500()            // db: error
     }
 }
 
@@ -85,6 +88,7 @@ fn main() -> io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug");
     env_logger::init();
 
+    // parse command line
     let cli_matches = clap::App::new("kvapp")
                       .version(VERSION)
                       .author("Jeff Garzik <jgarzik@pobox.com>")
@@ -96,30 +100,43 @@ fn main() -> io::Result<()> {
                            .takes_value(true))
                       .get_matches();
 
+    // configure based on CLI options
     let db_dir = cli_matches.value_of("config").unwrap_or(DEF_DB_DIR);
 
-    let sys = actix_rt::System::new("kvapp");
-
+    // configure & open db
     let db_config = ConfigBuilder::default()
         .path(db_dir)
         .use_compression(false);
     let db = Db::start(db_config.build()).unwrap();
 
+    // configure web server
+    let sys = actix_rt::System::new("kvapp");
+
     HttpServer::new(move || {
         App::new()
+            // pass application state to each handler
             .data(ServerState { db: db.clone() })
+
             // enable logger - always register actix-web Logger middleware last
             .wrap(middleware::Logger::default())
-            // register simple routes, handle all methods
+
+            // register our routes
             .service(index)
-            .service(web::resource("/1/db/{dbkey}").route(web::get().to(get)))
-            .service(web::resource("/1/db/{dbkey}/{dbval}").route(web::put().to(put)))
+            .service(
+                web::resource("/1/db/{dbkey}")
+                    .route(web::get().to(get))
+            )
+            .service(
+                web::resource("/1/db/{dbkey}/{dbval}")
+                    .route(web::put().to(put))
+            )
+
             // default
             .default_service(
                 // 404 for GET request
                 web::resource("")
                     .route(web::get().to(p404))
-                    // all requests that are not `GET`
+                    // all requests that are not `GET` -- redundant?
                     .route(
                         web::route()
                             .guard(guard::Not(guard::Get()))
