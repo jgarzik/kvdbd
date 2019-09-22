9f6f57eec447d782e1a18d36305f0a0301fb19fb
Jeff Garzik <jeff@bloq.com>
2019-09-22 02:12:56 +0000

    convert to actix web

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-#![feature(proc_macro_hygiene,decl_macro,rustc_private)]
 
-#[macro_use] extern crate rocket;
+#[macro_use] extern crate actix_web;
 #[macro_use] extern crate serde_derive;
 
-#[cfg(test)] mod tests;
+use std::{env, io};
+
+use actix_session::{CookieSession, Session};
+use actix_web::http::{StatusCode};
+use actix_web::{
+    guard, middleware, web, App, HttpRequest, HttpResponse, HttpServer,
+    Result,
+};
+use serde_json::json;
 
 use std::sync::Mutex;
 use std::collections::HashMap;
 
-use serde_json::json;
-
-use rocket::State;
-
-type DbMap = Mutex<HashMap<String, String>>;
+type DbMap = web::Data<Mutex<HashMap<String, String>>>;
 
 #[derive(Serialize, Deserialize)]
 struct DbEntry {
@@ -20,38 +23,124 @@ struct DbEntry {
     val: String
 }
 
-#[get("/<dbkey>")]
-fn get(dbkey: String, map: State<'_, DbMap>) -> Option<String> {
-    let hashmap = map.lock().unwrap();
-    match hashmap.get(&dbkey) {
-        Some(val) => Some(json!({"result": val}).to_string()),
-        _ => None
+/// simple index handler
+#[get("/")]
+fn index(state: DbMap, session: Session, req: HttpRequest) -> Result<HttpResponse> {
+    println!("{:?}", req);
+
+    // session
+    let mut counter = 1;
+    if let Some(count) = session.get::<i32>("counter")? {
+        println!("SESSION value: {}", count);
+        counter = count + 1;
+    }
+
+    // set counter to session
+    session.set("counter", counter)?;
+
+    // response
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("application/json")
+        .body(json!({
+            "name": "kvapp",
+            "version": "0.1.0"}).to_string()))
+}
+
+/// GET data item
+fn get(state: DbMap, session: Session, req: HttpRequest, path: web::Path<(String,)>) -> Result<HttpResponse> {
+    println!("{:?}", req);
+
+    // session
+    let mut counter = 1;
+    if let Some(count) = session.get::<i32>("counter")? {
+        println!("SESSION value: {}", count);
+        counter = count + 1;
+    }
+
+    // set counter to session
+    session.set("counter", counter)?;
+
+    let hashmap = state.lock().unwrap();
+    match hashmap.get(&path.0) {
+        Some(val) => Ok(HttpResponse::build(StatusCode::OK)
+                .content_type("application/json")
+                .body(json!({"result": val}).to_string())),
+        _ => Ok(HttpResponse::build(StatusCode::NOT_FOUND)
+            .content_type("application/json")
+            .body(json!({
+              "error": {
+                 "code" : -404,
+                  "message": "not found"}}).to_string()))
     }
 }
 
-#[put("/<dbkey>/<dbval>")]
-fn put(dbkey: String, dbval: String, map: State<'_, DbMap>) -> String {
-    let mut hashmap = map.lock().unwrap();
-    hashmap.insert(dbkey, dbval);
+/// PUT data item
+fn put(state: DbMap, session: Session, req: HttpRequest, path: web::Path<(String,String)>) -> Result<HttpResponse> {
+    println!("{:?}", req);
 
-    json!({"result": true}).to_string()
+    // session
+    let mut counter = 1;
+    if let Some(count) = session.get::<i32>("counter")? {
+        println!("SESSION value: {}", count);
+        counter = count + 1;
+    }
+
+    // set counter to session
+    session.set("counter", counter)?;
+
+    let mut hashmap = state.lock().unwrap();
+    hashmap.insert(path.0.clone(), path.1.clone());
+
+    let bodystr = json!({"result": true}).to_string();
+
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("application/json")
+        .body(bodystr))
 }
 
-#[catch(404)]
-fn not_found() -> String {
-    json!({"error": {
-        "code": -404,
-        "message": "URI not found"
-        }}).to_string()
+/// 404 handler
+fn p404() -> Result<HttpResponse> {
+    Ok(HttpResponse::build(StatusCode::NOT_FOUND)
+        .content_type("application/json")
+        .body(json!({
+            "error": {
+                "code" : -404,
+                 "message": "not found"}}).to_string()))
 }
 
-fn rocket() -> rocket::Rocket {
-    rocket::ignite()
-        .mount("/1/db", routes![get, put])
-        .register(catchers![not_found])
-        .manage(Mutex::new(HashMap::<String, String>::new()))
-}
+fn main() -> io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug");
+    env_logger::init();
+    let sys = actix_rt::System::new("kvapp");
+    let hmm = web::Data::new(Mutex::new(HashMap::<String, String>::new()));
 
-fn main() {
-    rocket().launch();
+    HttpServer::new(move || {
+        App::new()
+            .register_data(hmm.clone())
+            // cookie session middleware
+            .wrap(CookieSession::signed(&[0; 32]).secure(false))
+            // enable logger - always register actix-web Logger middleware last
+            .wrap(middleware::Logger::default())
+            // register simple routes, handle all methods
+            .service(index)
+            .service(web::resource("/1/db/{dbkey}").route(web::get().to(get)))
+            .service(web::resource("/1/db/{dbkey}/{dbval}").route(web::put().to(put)))
+            // default
+            .default_service(
+                // 404 for GET request
+                web::resource("")
+                    .route(web::get().to(p404))
+                    // all requests that are not `GET`
+                    .route(
+                        web::route()
+                            .guard(guard::Not(guard::Get()))
+                            .to(HttpResponse::MethodNotAllowed),
+                    ),
+            )
+    })
+    .bind("127.0.0.1:8080")?
+    .start();
+
+    println!("Starting http server: 127.0.0.1:8080");
+    sys.run()
 }
