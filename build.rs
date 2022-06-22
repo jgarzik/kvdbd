249c87616506e3532b70bcac72ef86398b336a1e
Jeff Garzik <jeff@bloq.com>
2022-06-22 16:10:38 -0400

    Upgrade to protobuf v3

--- build.rs
+++ build.rs
@@ -1,10 +1,7 @@
-extern crate protoc_rust;
-
 fn main() {
-    protoc_rust::Codegen::new()
-        .out_dir("src/protos")
-        .inputs(&["src/protos/pbapi.proto"])
-        .includes(&["src/protos"])
-        .run()
-        .expect("protoc");
+    protobuf_codegen::Codegen::new()
+        .cargo_out_dir("protos")
+        .include("src")
+        .input("src/protos/pbapi.proto")
+        .run_from_script();
 }
