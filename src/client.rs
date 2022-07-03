0751991b15cf1758f757238cc32a0581bd3d7dc3
Jeff Garzik <jeff@bloq.com>
2022-07-02 22:19:59 -0400

    Break up modules into smaller modules

---
+++ src/client.rs
@@ -0,0 +1,147 @@
+extern crate reqwest;
+use crate::codec;
+
+pub const API_BASEURI: &'static str = "/api";
+
+use crate::pbapi::{get_response, DbStatResponse, GetResponse, MutationRequest};
+use protobuf::{EnumOrUnknown, Message};
+use reqwest::StatusCode;
+
+pub struct KvdbClient {
+    client: reqwest::Client,
+    pub db_id: String,
+    pub endpoint: String,
+}
+
+impl KvdbClient {
+    pub fn new(endpoint_: String, db_id_: String) -> KvdbClient {
+        KvdbClient {
+            client: reqwest::Client::builder()
+                .danger_accept_invalid_certs(true)
+                .build()
+                .unwrap(),
+            db_id: db_id_,
+            endpoint: endpoint_,
+        }
+    }
+
+    pub async fn get1(&mut self, key: String) -> Option<Vec<u8>> {
+        let basepath = format!("{}{}/{}/", self.endpoint, API_BASEURI, self.db_id);
+        let get_url = format!("{}mget", basepath);
+
+        // encode get request
+        let out_bytes = codec::pbenc_get1_req(key.as_bytes(), false);
+
+        // exec get request; key1 should exist and match value, following batch
+        let resp_res = self
+            .client
+            .post(&get_url)
+            .body(out_bytes.clone())
+            .send()
+            .await;
+        match resp_res {
+            Ok(resp) => {
+                if resp.status() != StatusCode::OK {
+                    return None;
+                }
+
+                match resp.bytes().await {
+                    Ok(bytes) => match GetResponse::parse_from_bytes(&bytes) {
+                        Err(_e) => None,
+                        Ok(in_resp) => {
+                            if in_resp.magic != EnumOrUnknown::new(get_response::MagicNum::MAGIC)
+                                || in_resp.res.len() != 1
+                                || !in_resp.res[0].is_ok
+                            {
+                                None
+                            } else {
+                                Some(in_resp.res[0].val.clone())
+                            }
+                        }
+                    },
+                    Err(_e) => None,
+                }
+            }
+            Err(_e) => None,
+        }
+    }
+
+    pub async fn mutate(&mut self, mut_req: &MutationRequest) -> bool {
+        let basepath = format!("{}{}/{}/", self.endpoint, API_BASEURI, self.db_id);
+        let mutate_url = format!("{}mutate", basepath);
+
+        // encode mutation request
+        let out_bytes = mut_req.write_to_bytes().unwrap();
+
+        // exec mutation request
+        let resp_res = self.client.post(&mutate_url).body(out_bytes).send().await;
+        match resp_res {
+            Ok(resp) => {
+                if resp.status() == StatusCode::OK {
+                    match resp.text().await {
+                        Ok(_body) => true,
+                        Err(_e) => false,
+                    }
+                } else {
+                    false
+                }
+            }
+            Err(_e) => false,
+        }
+    }
+
+    pub async fn put1(&mut self, key: String, value: String) -> bool {
+        // encode put request
+        let out_req = codec::pbenc_mutate_ins1(key.as_bytes(), value.as_bytes());
+
+        self.mutate(&out_req).await
+    }
+
+    pub async fn del1(&mut self, key: String) -> bool {
+        let basepath = format!("{}{}/{}/", self.endpoint, API_BASEURI, self.db_id);
+        let del_url = format!("{}del", basepath);
+
+        // encode del request
+        let out_bytes = codec::pbenc_key_req(key.as_bytes());
+
+        // exec del request
+        let resp_res = self.client.post(&del_url).body(out_bytes).send().await;
+        match resp_res {
+            Ok(resp) => {
+                if resp.status() == StatusCode::OK {
+                    match resp.text().await {
+                        Ok(_body) => true,
+                        Err(_e) => false,
+                    }
+                } else {
+                    false
+                }
+            }
+            Err(_e) => false,
+        }
+    }
+
+    pub async fn stat(&mut self) -> Option<DbStatResponse> {
+        let basepath = format!("{}{}/{}/", self.endpoint, API_BASEURI, self.db_id);
+        let stat_url = format!("{}stat", basepath);
+
+        // exec db-stat request
+        let resp_res = self.client.get(&stat_url).send().await;
+        if resp_res.is_err() {
+            return None;
+        }
+        let resp = resp_res.unwrap();
+        if resp.status() != StatusCode::OK {
+            return None;
+        }
+
+        // decode protobuf list-of-keys response
+        match resp.bytes().await {
+            Ok(bytes) => match DbStatResponse::parse_from_bytes(&bytes) {
+                Err(_e) => None,
+                Ok(req) => Some(req),
+            },
+            Err(_e) => return None,
+        }
+    }
+}
---
+++ src/codec.rs
@@ -0,0 +1,127 @@
+use kvdbd::db;
+
+use crate::pbapi::{
+    db_stat_response, get_request, iter_request, iter_response, key_request, mutation_request,
+    update_request, DbStatResponse, GetOp, GetRequest, IterRequest, IterResponse, KeyRequest,
+    MutationRequest, UpdateRequest,
+};
+use protobuf::{EnumOrUnknown, Message};
+
+pub fn pbenc_key_req(key: &[u8]) -> Vec<u8> {
+    let mut out_msg = KeyRequest::new();
+    out_msg.magic = EnumOrUnknown::new(key_request::MagicNum::MAGIC);
+    out_msg.key = key.to_vec();
+    return out_msg.write_to_bytes().unwrap();
+}
+
+pub fn pbenc_get1_req(key: &[u8], skip_val: bool) -> Vec<u8> {
+    let mut out_msg = GetRequest::new();
+    out_msg.magic = EnumOrUnknown::new(get_request::MagicNum::MAGIC);
+
+    let mut out_op = GetOp::new();
+    out_op.key = key.to_vec();
+    out_op.skip_val = skip_val;
+    out_msg.ops.push(out_op);
+
+    return out_msg.write_to_bytes().unwrap();
+}
+
+pub fn pbenc_mutate_ins1(key: &[u8], val: &[u8]) -> MutationRequest {
+    let mut out_msg = MutationRequest::new();
+    out_msg.magic = EnumOrUnknown::new(mutation_request::MagicNum::MAGIC);
+
+    let mut out_upd = UpdateRequest::new();
+    out_upd.magic = EnumOrUnknown::new(update_request::MagicNum::MAGIC);
+    out_upd.key = key.to_vec();
+    out_upd.value = val.to_vec();
+    out_upd.is_insert = true;
+
+    out_msg.reqs.push(out_upd);
+
+    out_msg
+}
+
+pub fn pbenc_db_stat_resp(n_records: u64) -> Vec<u8> {
+    let mut out_msg = DbStatResponse::new();
+    out_msg.magic = EnumOrUnknown::new(db_stat_response::MagicNum::MAGIC);
+    out_msg.n_records = n_records;
+
+    return out_msg.write_to_bytes().unwrap();
+}
+
+pub fn pbenc_iter_resp(key_list: &db::api::KeyList) -> Vec<u8> {
+    let mut out_msg = IterResponse::new();
+    out_msg.magic = EnumOrUnknown::new(iter_response::MagicNum::MAGIC);
+
+    for key in &key_list.keys {
+        out_msg.keys.push(key.clone());
+    }
+    out_msg.list_end = key_list.list_end;
+
+    return out_msg.write_to_bytes().unwrap();
+}
+
+pub fn pbdec_iter_req(wiredata: &[u8]) -> Option<IterRequest> {
+    match IterRequest::parse_from_bytes(wiredata) {
+        Err(_e) => None,
+        Ok(req) => {
+            if req.magic != EnumOrUnknown::new(iter_request::MagicNum::MAGIC) {
+                None
+            } else {
+                Some(req)
+            }
+        }
+    }
+}
+
+pub fn pbdec_key_req(wiredata: &[u8]) -> Option<KeyRequest> {
+    match KeyRequest::parse_from_bytes(wiredata) {
+        Err(_e) => None,
+        Ok(req) => {
+            if req.magic != EnumOrUnknown::new(key_request::MagicNum::MAGIC) {
+                None
+            } else {
+                Some(req)
+            }
+        }
+    }
+}
+
+pub fn pbdec_update_req(wiredata: &[u8]) -> Option<UpdateRequest> {
+    match UpdateRequest::parse_from_bytes(wiredata) {
+        Err(_e) => None,
+        Ok(req) => {
+            if req.magic != EnumOrUnknown::new(update_request::MagicNum::MAGIC) {
+                None
+            } else {
+                Some(req)
+            }
+        }
+    }
+}
+
+pub fn pbdec_mget_req(wiredata: &[u8]) -> Option<GetRequest> {
+    match GetRequest::parse_from_bytes(wiredata) {
+        Err(_e) => None,
+        Ok(req) => {
+            if req.magic != EnumOrUnknown::new(get_request::MagicNum::MAGIC) {
+                None
+            } else {
+                Some(req)
+            }
+        }
+    }
+}
+
+pub fn pbdec_mutate_req(wiredata: &[u8]) -> Option<MutationRequest> {
+    match MutationRequest::parse_from_bytes(wiredata) {
+        Err(_e) => None,
+        Ok(req) => {
+            if req.magic != EnumOrUnknown::new(mutation_request::MagicNum::MAGIC) {
+                None
+            } else {
+                Some(req)
+            }
+        }
+    }
+}
