92cea0866baf82511e13e7bddbf9902640f9a2d8
Jeff Garzik <jeff@bloq.com>
2022-07-03 00:47:59 -0400

    add TLS generator script

---
+++ ssl/mkssl.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir ssl
+cd ssl
+openssl req -x509 \
+            -sha256 -days 356 \
+            -nodes \
+            -newkey rsa:2048 \
+            -subj "/CN=127.0.0.1/C=US/L=Atlanta" \
+            -keyout rootCA.key -out rootCA.crt 
+
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr -config ../csr.conf
+openssl x509 -req \
+    -in server.csr \
+    -CA rootCA.crt -CAkey rootCA.key \
+    -CAcreateserial -out server.crt \
+    -days 365 \
+    -sha256 -extfile ../cert.conf
